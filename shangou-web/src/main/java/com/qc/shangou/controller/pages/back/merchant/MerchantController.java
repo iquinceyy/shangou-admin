976a06457c9814b750654801fb6982321336f81b
Quincey <973409978@qq.com>
2020-06-04 00:13:05 +0800

    11111

--- shangou-web/src/main/java/com/qc/shangou/controller/pages/back/merchant/MerchantController.java
+++ shangou-web/src/main/java/com/qc/shangou/controller/pages/back/merchant/MerchantController.java
@@ -1,7 +1,12 @@
 package com.qc.shangou.controller.pages.back.merchant;
 
+import com.qc.shangou.dao.BusinessTypeDao;
+import com.qc.shangou.pojo.entity.BusinessType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
 
 /**
  * Author quincey
@@ -11,9 +16,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/pages/back/merchant")
 public class MerchantController {
 
+
+    @Resource
+    BusinessTypeDao businessTypeDao;
     @RequestMapping("addPre")
     String addMerchant(String name) {
         return  "/pages/back/merchant/merchant-addPre";
+    }
+
+
+    @RequestMapping("business")
+    @ResponseBody
+    BusinessType selectBusiness(Integer BusinessTypeId){
+        BusinessType businessType = businessTypeDao.selectByPrimaryKey(1);
+        System.out.println(businessType);
+        return businessTypeDao.selectByPrimaryKey(1);
     }
 
 }
